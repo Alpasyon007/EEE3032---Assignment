5ebd7f83001c5e38b40d62e3ec3224d8ac9238d1
Alp Bora Orgun <alpasyon07@gmail.com>
2022-11-06 19:55:45 +0200

    Clean up PrecisionRecall script slightly

--- src/extensions/PrecisionRecall.m
+++ src/extensions/PrecisionRecall.m
@@ -1,19 +1,23 @@
 function PrecisionRecall(allfiles, NIMG, dst, SHOW)
     rowCol = [];
+    numOfColsPerRow = [];
+
+    row = 1;
+    col = 0;
+
+    relevantResultNum = 0;
+    totalNumOfResults = 14;
+    precision = 0;
+    recall = 0;
+
     for i=1:NIMG
         rowNum = str2num(['uint8(',extractBefore(allfiles(i).name, "_"),')']);
         colNum = str2num(['uint8(',extractBefore(extractAfter(allfiles(i).name,  rowNum + "_"), "_s"),')']);
-    
-    %     fprintf("Row num: %d\n", rowNum);
-    %     fprintf("Col num: %d\n", colNum);
     
         rowCol = [rowCol ; [rowNum colNum]];
     end
     rowCol = sortrows(rowCol, 1);
     
-    row = 1;
-    col = 0;
-    numOfColsPerRow = [];
     for i=1:NIMG
         if row == rowCol(i, 1)
             if rowCol(i, 2) > col
@@ -27,27 +31,19 @@ function PrecisionRecall(allfiles, NIMG, dst, SHOW)
         numOfColsPerRow(row, 1) = row;
         row = rowCol(i, 1);
     end
-    
-    precisionRelevancy = [];
-    recallRelevancy = [];
-    
+
+    % Row Numbers for Top 15 Results
     for i=1:SHOW
         dst(i, 3) = str2num(['uint8(',extractBefore(allfiles(dst(i, 2)).name, "_"),')']);
     end
     
+    %
     for i=2:SHOW
-        precisionRelevancy(i, 1) = (dst(1, 3) == dst(i, 3));
+        relevantResultNum = relevantResultNum + (dst(1, 3) == dst(i, 3));
     end
     
-    precision = 0;
-    recall = 0;
-    
-    for i=2:SHOW
-        precision = precision + precisionRelevancy(i, 1);
-    end
-    
-    recall = precision/numOfColsPerRow(dst(1, 3), 2);
-    fprintf("Recall: %f, Correctly Returned %d/%d from the dataset\n", recall, precision, numOfColsPerRow(dst(1, 3), 2));
-    
-    precision = precision/14;
-    fprintf('Precision: %f, %d/%d correct out of all returned\n', precision, precision*14, 14);
+    precision = relevantResultNum/totalNumOfResults;
+    recall = (precision*14)/(numOfColsPerRow(dst(1, 3), 2));
+
+    fprintf('Precision: %f, %d/%d correct out of all returned\n', precision, relevantResultNum, totalNumOfResults);
+    fprintf("Recall: %f, Correctly Returned %d/%d from the dataset\n", recall, relevantResultNum, numOfColsPerRow(dst(1, 3), 2));
